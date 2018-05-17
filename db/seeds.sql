34ee24870f44b1a47d4f9bd9cd8d8b0e0f8675aa
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-05-17 11:58:34 -0400

    added all directories and files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burgername, devoured) VALUES ('Bacon Cheeseburgers', true);
+
+INSERT INTO burgers (burgername, devoured) VALUES ('Egg Burger', true);
+
+INSERT INTO burgers (burgername, devoured) VALUES ('Veggie Burger', true);
