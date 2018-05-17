b194aa010b5d699b3380bb9f2de0be91585c9fe5
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-05-17 12:13:16 -0400

    added heroku

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-// Pull in required dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
+
+app.listen(port, function() {
+
 
 var app = express();
 
-// Serve static content for the app from the 'public' directory
-app.use(express.static(process.cwd() + '/public'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
+// parse application/json
+app.use(bodyParser.json());
 
-// Set Handlebars as the view engine
-var exphbs = require('express-handlebars');
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them
-var routes = require('./controllers/burgers_controller.js');
+// Import routes and give the server access to them.
+var routes = require("./controller/burgers_controllers.js");
 
-app.use('/', routes);
+// app.use(routes);
 
-app.listen(port);
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
